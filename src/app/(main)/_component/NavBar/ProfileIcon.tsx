fc9977db74676cce000f6b9b96e346223cfe65a4
minkyung_kwak <ellen10min@gmail.com>
2024-09-07 00:42:54 +0900

    #28 Add: middleware 추가, session 추가

--- src/app/(main)/_component/NavBar/ProfileIcon.tsx
+++ src/app/(main)/_component/NavBar/ProfileIcon.tsx
@@ -1,8 +1,19 @@
 'use client';
+
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import MenuItem from './MenuItem';
 
-const ProfileIcon = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
+const ProfileIcon = () => {
+  const { data: session, status } = useSession();
+
+  if (status === 'loading') {
+    return <div>{'Loading...'}</div>;
+  }
+
+  const isLoggedIn = status === 'authenticated';
+  const userImg = session?.user?.image;
+
   if (!isLoggedIn) {
     return (
       <div>
@@ -17,13 +28,17 @@ const ProfileIcon = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
     );
   }
 
+  const imageSrc = userImg || '/images/gappler.png';
+
   return (
-    <div className="flex items-center justify-center m-4">
-      <Link href="/profile">
+    <div className={'flex items-center justify-center m-4'}>
+      <Link href={'/profile'}>
         <div
-          className="w-10 h-10 rounded-full tablet:w-8 tablet:h-8"
+          className={
+            'w-10 laptop:w-12 laptop:h-12 h-10 rounded-full tablet:w-8 tablet:h-8'
+          }
           style={{
-            backgroundImage: 'url(/images/ham.jpeg)',
+            backgroundImage: `url('${imageSrc}')`,
             backgroundSize: 'cover',
             backgroundPosition: 'center',
             border: '2px solid white',
