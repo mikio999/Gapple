f457b772f33910fa8ec8f232557d7dc6a6c5f148
minkyung_kwak <ellen10min@gmail.com>
2024-09-19 00:18:50 +0900

    #39 Add: record Page template 제작

--- src/app/record/page.tsx
+++ src/app/record/page.tsx
@@ -1,5 +1,14 @@
+import CreateRecord from './_component/CreateRecord';
+
 const RecordPage = () => {
-  return <h1>{'수업 기록지 페이지'}</h1>;
+  return (
+    <div>
+      <h1 className={'text-xl px-4 font-maple text-gray700'}>
+        {'수업 기록지 페이지'}
+      </h1>
+      <CreateRecord />
+    </div>
+  );
 };
 
 export default RecordPage;
