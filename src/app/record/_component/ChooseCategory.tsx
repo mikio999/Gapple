f57300ede0e5858c9645b74846458b4b1920ee29
minkyung_kwak <ellen10min@gmail.com>
2024-10-06 19:59:27 +0900

    #55 Add: CreateRecord Category 정립

---
+++ src/app/record/_component/ChooseCategory.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React, { useState } from 'react';
+import { category } from '@/_lib/constants/category';
+import CategorySelect from '@/app/lessonForm/_component/CategorySelect';
+
+export default function ChooseCategory() {
+  const [selectCategory, setSelectCategory] = useState('');
+  const handleCategorySelect = (value: string) => {
+    setSelectCategory(value);
+    console.log(`선택된 카테고리: ${selectCategory}`);
+  };
+  return <CategorySelect options={category} onSelect={handleCategorySelect} />;
+}
