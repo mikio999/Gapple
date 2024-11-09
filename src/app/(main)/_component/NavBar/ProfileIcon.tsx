31f070c5e3ec9d4978927a2bc1c439da5c1c21be
minkyung_kwak <ellen10min@gmail.com>
2024-11-09 12:18:20 +0900

    #Modify Add: redirect url

--- src/app/(main)/_component/NavBar/ProfileIcon.tsx
+++ src/app/(main)/_component/NavBar/ProfileIcon.tsx
@@ -18,7 +18,8 @@ const DropdownMenu = () => {
       }
     >
       <Link
-        href={`/profile/${session.userId}/plan`}
+        href={'/'}
+        // href={`/profile/${session.userId}/plan`}
         className={
           'block px-4 py-2 text-center text-sm text-slate-700 hover:bg-slate-100'
         }
