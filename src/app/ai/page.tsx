950bcbeabc1536f8705bf92a3e330adbc045453e
minkyung_kwak <ellen10min@gmail.com>
2024-12-14 00:20:43 +0900

    #104 Style: CSS mobile 최적화 적용

--- src/app/ai/page.tsx
+++ src/app/ai/page.tsx
@@ -2,7 +2,11 @@ import AiManager from './_component/AiManager';
 
 const AiPage = () => {
   return (
-    <div className={'w-full bg-slate-300 p-8'}>
+    <div
+      className={
+        'min-h-screen w-full bg-slate-300 p-8 flex flex-col items-center'
+      }
+    >
       <AiManager />
     </div>
   );
