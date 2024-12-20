5f0ecbd2897197215da8452db630501fe5fc7d9e
minkyung_kwak <ellen10min@gmail.com>
2024-12-20 14:56:44 +0900

    #123 Modify: Axios 변환

--- src/app/(main)/_component/OneSignal/utils/sendSubscriptionToServer.ts.ts
+++ src/app/(main)/_component/OneSignal/utils/sendSubscriptionToServer.ts.ts
@@ -1,23 +1,37 @@
+import axios from 'axios';
+
 export async function sendSubscriptionToServer(
   subscriptionId: string,
   accessToken: string,
 ) {
   try {
-    const response = await fetch('/api/onesignal', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${accessToken}`,
-      },
-      body: JSON.stringify({ subscriptionId }),
-    });
+    console.log('보내는 데이터 확인:');
+    console.log('subscriptionId:', subscriptionId);
+    console.log('{subscriptionId}:', { subscriptionId });
+    console.log('accessToken:', accessToken);
 
-    if (response.ok) {
+    const response = await axios.post(
+      '/api/onesignal',
+      { subscriptionId },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
+
+    console.log('서버 응답:', response.status, response.data);
+
+    if (response.status === 200) {
       console.log('구독 정보 저장 성공!');
     } else {
       console.error('구독 정보 저장 실패');
     }
-  } catch (error) {
-    console.error('구독 정보 서버 전송 중 에러 발생:', error);
+  } catch (error: any) {
+    console.error(
+      '구독 정보 서버 전송 중 에러 발생:',
+      error.response?.data || error.message,
+    );
   }
 }
